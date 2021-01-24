ba211de058e2259610bdb98c5d457dc620da5fa4
Praveen Oruganti <praveenorugantitech@gmail.com>
2021-01-24 23:57:21 +0530

    Updates

---
+++ precache-manifest.ef1797f363ac28eec3aae709b44afb39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f41901bfeb1744ef8deead667c217ce5",
+    "url": "/praveenorugantitech-countdown-timer-reactjs/index.html"
+  },
+  {
+    "revision": "672a9603174f214d5471",
+    "url": "/praveenorugantitech-countdown-timer-reactjs/static/css/main.379834ca.chunk.css"
+  },
+  {
+    "revision": "0786b8e030352c28e22d",
+    "url": "/praveenorugantitech-countdown-timer-reactjs/static/js/2.47e02a0e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/praveenorugantitech-countdown-timer-reactjs/static/js/2.47e02a0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "672a9603174f214d5471",
+    "url": "/praveenorugantitech-countdown-timer-reactjs/static/js/main.c4b82ff5.chunk.js"
+  },
+  {
+    "revision": "daa21dc15d9fc42e1743",
+    "url": "/praveenorugantitech-countdown-timer-reactjs/static/js/runtime-main.772130ec.js"
+  },
+  {
+    "revision": "4c940fa9707ba8042ca818a9e7c94296",
+    "url": "/praveenorugantitech-countdown-timer-reactjs/static/media/newyear.4c940fa9.jpg"
+  }
+]);
